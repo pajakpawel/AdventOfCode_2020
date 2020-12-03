7763dfc8d87c075f9096066185ca9a6c4b7dfc16
pajakpawel <pajakpawel@vp.pl>
2020-12-03 14:22:19 +0100

    Added puzzle input handling for puzzle 1 solution.

--- Day_1/Puzzle_1.py
+++ Day_1/Puzzle_1.py
@@ -3,3 +3,12 @@ def expense_report_fix(dataset: list) -> int:
         for number_2 in dataset[number_1_index + 1:]:
             if number_1 + number_2 == 2020:
                 return number_1 * number_2
+
+
+if __name__ == '__main__':
+    puzzle_dataset = []
+    with open("puzzle_input.txt") as puzzle_input:
+        for number in puzzle_input.readlines():
+            puzzle_dataset.append(int(number))
+
+    print("Solution for dataset included in './puzzle_input.txt' is equal to ", expense_report_fix(puzzle_dataset))
