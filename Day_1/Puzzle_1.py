84a09228364f32c029f8dd4f3b3fa45db2c1ec95
pajakpawel <pajakpawel@vp.pl>
2020-12-03 13:27:30 +0100

    Implemented body for expense_report_fix function.

--- Day_1/Puzzle_1.py
+++ Day_1/Puzzle_1.py
@@ -1,2 +1,5 @@
 def expense_report_fix(dataset: list) -> int:
-    pass
+    for number_1_index, number_1 in enumerate(dataset):
+        for number_2 in dataset[number_1_index + 1:]:
+            if number_1 + number_2 == 2020:
+                return number_1 * number_2
