68952e1c60385893137a38dc071ad676384631d9
pajakpawel <pajakpawel@vp.pl>
2020-12-07 14:56:39 +0100

    Created global input handler to avoid code redundancy.

---
+++ Input_handler.py
@@ -0,0 +1,12 @@
+from typing import Callable, List, Any
+
+
+def handle_input_for_given_function(filepath: str, function: Callable[[List[str]], Any]) -> None:
+    puzzle_dataset = []
+    with open(filepath) as puzzle_input:
+        for line in puzzle_input.readlines():
+            puzzle_dataset.append(line)
+
+    solution = function(puzzle_dataset)
+    print("Solution for dataset included in '{filepath}' is equal to {solution}".format(filepath=filepath,
+                                                                                        solution=solution))
