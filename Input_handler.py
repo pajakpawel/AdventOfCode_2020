4fc95d705eb73b762b0d52c0f7e2742ee67bf2bc
pajakpawel <pajakpawel@vp.pl>
2020-12-11 00:27:54 +0100

    Deleted spare code lines from input handler.

--- Input_handler.py
+++ Input_handler.py
@@ -2,11 +2,8 @@ from typing import Callable, List, Any
 
 
 def handle_input_for_given_function(filepath: str, function: Callable[[List[str]], Any]) -> None:
-    puzzle_dataset = []
     with open(filepath) as puzzle_input:
         puzzle_dataset = puzzle_input.read().splitlines()
-        # for line in puzzle_input.readlines():
-        #     puzzle_dataset.append(line)
 
     solution = function(puzzle_dataset)
     print("Solution for dataset included in '{filepath}' is equal to {solution}".format(filepath=filepath,
