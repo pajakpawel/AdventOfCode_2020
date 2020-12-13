44da84bf5d29abb4a1ddd7be4e4079521cc80f13
pajakpawel <pajakpawel@vp.pl>
2020-12-13 20:03:11 +0100

    Created unit tests for puzzle 1.

---
+++ Day_6/Test_Puzzle_1.py
@@ -0,0 +1,15 @@
+import unittest
+from Day_6 import Puzzle_1
+
+
+class TestFirstPuzzle(unittest.TestCase):
+    def test_count_positive_answers(self):
+        group_answers = "abcx\nabcy\nabcz"
+        self.assertEqual(6, Puzzle_1.count_positive_answers(group_answers))
+
+    def test_count_positive_answers_sum(self):
+        self.assertEqual(11, Puzzle_1.count_positive_answers_sum('puzzle_1_test_input.txt'))
+
+
+if __name__ == '__main__':
+    unittest.main()
