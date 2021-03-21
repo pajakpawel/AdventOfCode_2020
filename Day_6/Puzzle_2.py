25365995e749a4e64398e7fb302fcbf814c26bf7
pajakpawel <pajakpawel@vp.pl>
2021-03-21 22:26:30 +0100

    Created puzzle 2 solution file with functions declarations.

---
+++ Day_6/Puzzle_2.py
@@ -0,0 +1,10 @@
+def count_group_positive_answers(group_answers: str) -> int:
+    pass
+
+
+def sum_up_groups_positive_answers(filepath: str) -> int:
+    pass
+
+
+if __name__ == '__main__':
+    pass
